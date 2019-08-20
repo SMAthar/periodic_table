4be72af38afe4cb3e797eb49dfdebd3af1bee820
SMAthar <47472554+SMAthar@users.noreply.github.com>
2019-08-20 19:28:01 +0530

    Some changes

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:priodic_table/model/element.dart' as elem;
-import 'package:priodic_table/pages/table_view.dart';
 import 'package:priodic_table/widgets/nevigation_tab.dart';
 
-class DetailsPage extends StatefulWidget {
+class DetailsPage extends StatelessWidget {
   final elem.Element element;
   final elem.ElementList elementList;
 
@@ -13,31 +12,17 @@ class DetailsPage extends StatefulWidget {
   });
 
   @override
-  _DetailsPageState createState() => _DetailsPageState();
-}
-
-class _DetailsPageState extends State<DetailsPage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: Colors.black45,
-        title: Text(widget.element.name),
-        leading: IconButton(
-          icon: Icon(
-            Icons.arrow_back,
-            color: Colors.white,
-          ),
-          onPressed: (){
-            Navigator.push(context,MaterialPageRoute(builder: (context) => TableView(elementList: widget.elementList,)));
-          },
-        ),
+        title: Text(element.name),
       ),
       body: ListView(
         children: <Widget>[
           Image.asset(
-            widget.element.imgPath,
+            element.imgPath,
             fit: BoxFit.cover,
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height * 0.3,
@@ -45,8 +30,8 @@ class _DetailsPageState extends State<DetailsPage> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              widget.element.atomicNo == 1 ? DammyNT() : NavigationTab(element: widget.elementList.elements[widget.element.atomicNo - 2],elementList: widget.elementList,),
-              widget.element.atomicNo == 118 ? DammyNT() : NavigationTab(element: widget.elementList.elements[widget.element.atomicNo],elementList: widget.elementList,)
+              element.atomicNo == 1 ? DammyNT() : NavigationTab(element: elementList.elements[element.atomicNo - 2],elementList: elementList,),
+              element.atomicNo == 118 ? DammyNT() : NavigationTab(element: elementList.elements[element.atomicNo],elementList: elementList,)
             ],
           )
         ],
